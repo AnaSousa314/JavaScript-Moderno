640330d9e3c00cd789f73242fa29e611d863a3a5
AnaSousa <anasousa314@outlook.com>
2021-07-25 18:07:56 -0300

    aula02 sobre parametros com valor default em funcoes

---
+++ 02-Parametros_default_em_funcoes/02-parameters.js
@@ -0,0 +1,7 @@
+function printGreetings(message = 'Mensagem padrão'){
+  console.log(message);
+}
+
+// printGreetings('Seja muito bem vindo!');
+
+printGreetings();//vai retornar o valor dado como default no parametro da funcao, no caso "Mensagem padrão"
